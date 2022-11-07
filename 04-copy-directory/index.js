b163b93067548b660f98a914da40c9e61bee5df2
JaLongin <neznayupochemu1@gmail.com>
2022-11-07 22:04:42 +0300

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const { readdir } = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+const pathToCopies = path.join(__dirname, "files-copy");
+const pathToOriginals = path.join(__dirname, "files");
+(async function(){
+    try{
+    const files = await fsPromises.readdir(pathToOriginals);
+    const pathsToFiles = files.map(file =>path.join(pathToOriginals, file));
+    console.log(pathsToFiles)
+    fsPromises.mkdir(pathToCopies,{recursive: true});
+    pathsToFiles.forEach((pathy, index) => {(async function(){
+        try{
+            console.log(pathy, path.join(pathToCopies, files[index]))
+            await fsPromises.copyFile(pathy, path.join(pathToCopies, files[index]));
+            console.log('file was copied successfully')
+        }catch{
+            console.log('file was not copied')
+        }
+    })()})
+    }catch(e){
+
+    }
+})();
+console.log(pathToCopies);
