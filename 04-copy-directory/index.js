a3188a3eac89daf380ccd1d9dd1631e344824d10
JaLongin <neznayupochemu1@gmail.com>
2022-11-08 22:37:07 +0300

    fix: fix actualization of files-copy folder in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,8 +5,11 @@ const pathToCopies = path.join(__dirname, "files-copy");
 const pathToOriginals = path.join(__dirname, "files");
 (async function(){
     try{
-    
-    fsPromises.mkdir(pathToCopies,{recursive: true});
+    await fsPromises.mkdir(pathToCopies,{recursive: true});
+    let rubbish = await fsPromises.readdir(pathToCopies);
+    for (i = 0; i < rubbish.length; i++){
+        await fsPromises.rm(path.join(pathToCopies, rubbish[i]));
+    }
     const files = await fsPromises.readdir(pathToOriginals);
     const pathsToFiles = files.map(file =>path.join(pathToOriginals, file));
     pathsToFiles.forEach((pathy, index) => {(async function(){
